1d66441d3db553bdb67fa61f68392e7e0733f1ba
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 12:23:07 +0400

    Task2_Tsitsikashvili

---
+++ src/test/java/Task2.java
@@ -0,0 +1,13 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int age = 28;
+        double height = 1.60;
+        char initial = 'N';
+        boolean isStudent = false;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(initial);
+        System.out.println(isStudent);
+    }
+}
