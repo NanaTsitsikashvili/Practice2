1d5025120b8408db09fe364d72f317731b1a4fc2
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 13:57:23 +0400

    Task3_Tsitsikashvili

---
+++ src/test/java/Task3.java
@@ -0,0 +1,18 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int num1 = 30;
+        int num2 = 6;
+
+        int jami = num1 + num2;
+        int gamokleba = num1 - num2;
+        int gamravleba = num1 * num2;
+        double gakopa =  num1 / num2;
+        int nashti = num1 % num2;
+
+        System.out.println( jami);
+        System.out.println(gamokleba);
+        System.out.println( gamravleba);
+        System.out.println(gakopa);
+        System.out.println(nashti);
+    }
+}
