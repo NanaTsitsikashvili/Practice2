fdf7ba0340f867c43b897a5dcf2b43cf73f93f7a
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 13:59:31 +0400

    Task4_Tsitsikashvili

---
+++ src/test/java/Task4.java
@@ -0,0 +1,16 @@
+public class Task4 {
+    public static void main(String[] args) {
+        String fullName = "Nana Tsitsikashvili";
+
+        System.out.println("Hello, my name " + fullName);
+
+        int nameLength = fullName.length();
+
+        System.out.println("სიმბოლოების რაოდენობა " + nameLength);
+
+        String upperCaseName = fullName.toUpperCase();
+
+        System.out.println("upper case " + upperCaseName);
+
+    }
+}
