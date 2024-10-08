340c19bae7274ffc143c1a61686e4b34c5902902
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 14:05:29 +0400

    Task7_Tsitsikashvili

---
+++ src/test/java/Task7.java
@@ -0,0 +1,16 @@
+public class Task7 {
+    public static void main(String[] args) {
+        int result = 70;
+        result += 10;
+        System.out.println("10 - ის დამატება " + result);
+
+        result -= 5;
+        System.out.println("5-ის გამოკლება " + result);
+
+        result *= 2;
+        System.out.println("2-ზე გამრავლების შემდეგ: " + result);
+
+        result /= 3;
+        System.out.println("3-ზე გაყოფის შემდეგ: " + result);
+    }
+}
