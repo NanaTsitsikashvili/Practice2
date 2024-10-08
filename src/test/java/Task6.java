dd47ef70dd2299a5a7396525ed0bdc4afe720211
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 15:43:43 +0400

    Task9_Tsitsikashvili

--- src/test/java/Task6.java
+++ src/test/java/Task6.java
@@ -1,7 +1,8 @@
 public class Task6 {
     public static void main(String[] args) {
         double radiusi = 10.0;
-        double area = Math.PI * radiusi * radiusi;
+        double pi = 3.142, area;
+        area = pi * radiusi * radiusi;
 
         System.out.println("წრის ფართობი: " + area);
     }
---
+++ src/test/java/Task9.java
@@ -0,0 +1,9 @@
+public class Task9 {
+    public static void main(String[] args) {
+        int a = 10;
+        a = a * 2 - 5;
+        boolean shedegi1 = (a > 15) && (a < 20);
+
+        System.out.println("shedegi " + shedegi1);
+    }
+}
