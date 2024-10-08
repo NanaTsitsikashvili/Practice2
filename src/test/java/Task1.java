9b6de8d5ff18dc7c04bbe4d02c109b6a4113745f
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-10-08 12:20:52 +0400

    Task1_Tsitsikashvili

---
+++ src/test/java/Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println("Nana Tsitsikashvili");
+        System.out.println("Age: 28");
+        System.out.println("Favourite Language: Java");
+    }
+}
